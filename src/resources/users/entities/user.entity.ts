bdb34ea7e2d60cdef3c5ee044dec4f92bf90ac42
dosepehr <dosepehr@gmail.com>
2025-02-01 11:38:36 +0330

    add users entity

--- src/resources/users/entities/user.entity.ts
+++ src/resources/users/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import { Exclude } from 'class-transformer';
+import { UserRole } from 'src/utils/enums/UserRole.enum';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ unique: true })
+  mobile: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
