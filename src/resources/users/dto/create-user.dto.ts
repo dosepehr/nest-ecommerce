10084c35bfa90dee3e597f21361c43c81fcebbf1
dosepehr <dosepehr@gmail.com>
2025-02-01 11:52:26 +0330

    add createUserDto

--- src/resources/users/dto/create-user.dto.ts
+++ src/resources/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { Transform } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsString,
+  Length,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+export class CreateUserDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(15)
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(11, 11)
+  @Transform(({ value }) => value.trim())
+  mobile: string;
+
+  @MinLength(8)
+  @MaxLength(20)
+  password: string;
+}
