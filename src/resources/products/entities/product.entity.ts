b02c3ea017791599ec7abf901a850acb82228b4e
dosepehr <dosepehr@gmail.com>
2025-02-17 14:11:31 +0330

    add products entity

--- src/resources/products/entities/product.entity.ts
+++ src/resources/products/entities/product.entity.ts
@@ -1,1 +1,35 @@
-export class Product {}
+import { Category } from 'src/resources/categories/entities/category.entity';
+import { User } from 'src/resources/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  price: string;
+
+  @Column()
+  stock: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+}
