dfb9fa2e71dfe4f6bd2935fcc127a3a303029fed
dosepehr <dosepehr@gmail.com>
2025-02-18 10:08:09 +0330

    add creating products

--- src/resources/products/products.service.ts
+++ src/resources/products/products.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { Category } from '../categories/entities/category.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+  async create(data: CreateProductDto) {
+    const category = await this.categoryRepository.findOneBy({
+      id: data.category,
+    });
+    if (!category) {
+      throw new NotFoundException('No Category Found');
+    }
+    const product = this.productRepository.create({ ...data, category });
+    return this.productRepository.save(product);
   }
 
   findAll() {
